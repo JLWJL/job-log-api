9cdcc9d7796cfb8590e59a01563a51c874879318
Junlong Wang <wjlinnz@gmail.com>
2017-10-27 10:51:24 +1300

    Updated createJob function and formatted code style - Turns empty expire input as null to DB

--- api/models/jobModel.js
+++ api/models/jobModel.js
@@ -1,85 +1,90 @@
-'use strict';
+"use strict";
 
-const db = require('../../config/db');
+const db = require("../../config/db");
 
-function getJobs(values, done){
-	let sql = "Select * FROM application where user_id = ?";
-	db.getPool().query(sql, [values], (err, results, fields)=>{
-		if(err) {
-			return done({
-				"message": err,
-				"status":500
-			}, results);
-		}
-		return done(err, results);
-	});
-}
-
-/*
-* Just for development test purpose*/
-function listJobs(done){
-  let sql = "Select * FROM application";
-  db.getPool().query(sql, (err, results, fields)=>{
-    if(err) {
+function getJobs(values, done) {
+  let sql = "Select * FROM application where user_id = ?";
+  db.getPool().query(sql, [values], (err, results, fields) => {
+    if (err) {
       return done({
         "message": err,
-        "status":500
+        "status": 500,
       }, results);
     }
     return done(err, results);
   });
 }
 
-
-function singleJob(values, done){
-	let id = db.getPool().escape(values);
-	let sql = "SELECT * FROM application WHERE app_id ="+id;
-	
-	db.getPool().query(sql,(err,results,fields)=>{
-		if(err){
-			return done({
-				"message": err,
-				"status":500
-			}, results);
-		}
-		return done(err, results);
-	});
+/*
+* Just for development test purpose*/
+function listJobs(done) {
+  let sql = "Select * FROM application";
+  db.getPool().query(sql, (err, results, fields) => {
+    if (err) {
+      return done({
+        "message": err,
+        "status": 500,
+      }, results);
+    }
+    return done(err, results);
+  });
 }
 
+function singleJob(values, done) {
+  let id = db.getPool().escape(values);
+  let sql = "SELECT * FROM application WHERE app_id =" + id;
 
-function createJob(values, done){
-	let sql = "INSERT INTO application SET ?";
-	
-	db.getPool().query(sql,[values], (err,results,fields)=>{
-		if(err){
-			return done({
-				"message": err,
-				"status":500
-			}, results);
-		}
-		return done(err, results);
-	})
+  db.getPool().query(sql, (err, results, fields) => {
+    if (err) {
+      return done({
+        "message": err,
+        "status": 500,
+      }, results);
+    }
+    return done(err, results);
+  });
 }
 
+/**
+ * Create new job application recrd
+ * @param {Object} values Key-Value pair of form input data
+ * @param {function} done
+ */
+function createJob(values, done) {
 
-function deleteJob(values, done){
-	let sql = "DELETE FROM application WHERE app_id = ?";
-	
-	db.getPool().query(sql,[values], (err,results,fields)=>{
-		if(err){
-			return done({
-				"message": err,
-				"status":500
-			}, results);
-		}
-		return done(err, results);
-	})
+  let sql = "INSERT INTO application SET ?";
+
+  //Empty expire counts as empty string which is invalid in DB column
+  if (values.expire === "") values.expire = null;
+  db.getPool().query(sql, [values], (err, results, fields) => {
+    if (err) {
+      return done({
+        "message": err,
+        "status": 500,
+      }, results);
+    }
+    return done(err, results);
+  });
 }
 
-module.exports={
+function deleteJob(values, done) {
+  let sql = "DELETE FROM application WHERE app_id = ?";
+
+  db.getPool().query(sql, [values], (err, results, fields) => {
+    if (err) {
+      return done({
+        "message": err,
+        "status": 500,
+      }, results);
+    }
+    return done(err, results);
+  });
+}
+
+module.exports = {
   listJobs: listJobs,
   getJobs: getJobs,
-	singleJob: singleJob,
-	createJob: createJob,
-	deleteJob: deleteJob
+  singleJob: singleJob,
+  createJob: createJob,
+  deleteJob: deleteJob,
 };
